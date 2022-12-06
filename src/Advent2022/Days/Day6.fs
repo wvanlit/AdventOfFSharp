4f89f8db012fe86c9d20acb57d30400e4d48e4b1
Wessel van Lit <wessel@litdev.nl>
2022-12-06 07:55:05 +0100

    Day 6

---
+++ src/Advent2022/Days/Day6.fs
@@ -0,0 +1,15 @@
+﻿module Advent2022.Days.Day6
+
+open System
+
+let findIndexOfNonRecurring n (input: string) =
+    input.ToCharArray ()
+    |> Array.windowed n
+    |> Array.map Set.ofArray
+    |> Array.findIndex (fun set -> Set.count set = n)
+    |> fun index -> index + n
+
+
+let Part1 input = findIndexOfNonRecurring 4 input
+
+let Part2 input = findIndexOfNonRecurring 14 input
---
+++ test/Advent2022Tests/Days/Day6.fs
@@ -0,0 +1,27 @@
+﻿module Advent2022Tests.Days.Day6
+
+open Advent2022.Days.Day6
+open Helpers
+open Helpers.Tests
+open Xunit
+
+let day = 6
+
+let input: TestInput<int> =
+    { Part1 = { Example = 7; Puzzle = 1238 }
+      Part2 = { Example = 19; Puzzle = -2 } }
+
+let example = PuzzleInput.load 2022 day PuzzleInput.Type.Example
+let puzzle = PuzzleInput.load 2022 day PuzzleInput.Type.Puzzle
+
+[<Fact>]
+let ``Part 1 - Example`` () = Assert.Equal (input.Part1.Example, Part1 example)
+
+[<Fact>]
+let ``Part 1 - Puzzle`` () = Assert.Equal (input.Part1.Puzzle, Part1 puzzle)
+
+[<Fact>]
+let ``Part 2 - Example`` () = Assert.Equal (input.Part2.Example, Part2 example)
+
+[<Fact>]
+let ``Part 2 - Puzzle`` () = Assert.Equal (input.Part2.Puzzle, Part2 puzzle)
